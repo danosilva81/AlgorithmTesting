d61d14b4af2af5b333c9021d9f41173c4455b3c8
Daniel Silva <djimenez@qubeyond.com>
2020-11-17 18:12:40 -0300

    exercise 2

---
+++ TenerifeApp.Test/Exercise2_Test.cs
@@ -0,0 +1,95 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TenerifeApp.Test
+{
+    public class Exercise2_Test
+    {
+        [Test]
+        public void Test_RoundsInCasino_1()
+        {
+            var numTest = new Exercise2();
+            var result = numTest.RoundsInCasino(8,0);
+            Assert.AreEqual(7, result);
+        }
+
+        [Test]
+        public void Test_RoundsInCasino_2()
+        {
+            var numTest = new Exercise2();
+            var result = numTest.RoundsInCasino(10, 10);
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Test_RoundsInCasino_3()
+        {
+            var numTest = new Exercise2();
+            var result = numTest.RoundsInCasino(20, 5);
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Test_RoundsInCasino_4()
+        {
+            var numTest = new Exercise2();
+            var result = numTest.RoundsInCasino(20, 2);
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void Test_RoundsInCasino_5()
+        {
+            var numTest = new Exercise2();
+            var result = numTest.RoundsInCasino(21, 6);
+            Assert.AreEqual(6, result);
+        }
+
+        //TESTEANDO AL FLACO
+        [Test]
+        public void Test_MinimumRoundsInCasino_1()
+        {
+            var numTest = new Exercise2();
+            var result = numTest.MinimumRoundsInCasino(8, 0);
+            Assert.AreEqual(7, result);
+        }
+
+        [Test]
+        public void Test_MinimumRoundsInCasino_2()
+        {
+            var numTest = new Exercise2();
+            var result = numTest.MinimumRoundsInCasino(10, 10);
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Test_MinimumRoundsInCasino_3()
+        {
+            var numTest = new Exercise2();
+            var result = numTest.RoundsInCasino(20, 5);
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Test_MinimumRoundsInCasino_4()
+        {
+            var numTest = new Exercise2();
+            var result = numTest.RoundsInCasino(20, 2);
+            Assert.AreEqual(6, result);
+        }
+
+
+        [Test]
+        public void Test_MinimumRoundsInCasino_5()
+        {
+            var numTest = new Exercise2();
+            var result = numTest.RoundsInCasino(21, 6);
+            Assert.AreEqual(6, result);
+        }
+
+        //TESTEANDO AL JIMMY
+
+    }
+}
--- TenerifeApp/Exercise2.cs
+++ TenerifeApp/Exercise2.cs
@@ -6,5 +6,43 @@ namespace TenerifeApp
 {
     public class Exercise2
     {
+        public int RoundsInCasino(int N, int K) 
+        {
+            if(N == 2)
+                return 1;
+
+            if (K == 0)
+                return N - 1;
+
+            var partial = (N % 2 != 0) ? 2 : 1;
+
+            return partial + RoundsInCasino(N / 2, K - 1);
+        }
+
+        //Solucion del Flaco
+        public int MinimumRoundsInCasino(int n, int k)
+        {
+            if ((k is 0) || (n is 2)) return n - 1;
+
+            return (n % 2 is 0)
+                ? 1 + MinimumRoundsInCasino(n / 2, k - 1)
+                : 1 + MinimumRoundsInCasino(n - 1, k);
+        }
+
+        //Solución del Jimmy
+        public int Solution(int totalEarned, int maximunAllIn)
+        {
+            if (totalEarned == 1) return 0;
+            return GetMinimunBet(totalEarned, maximunAllIn);
+        }
+
+        private int GetMinimunBet(int totalEarned, int maximunAllIn)
+        {
+            if (totalEarned == 2 || maximunAllIn == 0) return totalEarned - 1;
+            var hasWinWithAllIn = totalEarned % 2 == 0;
+            if (hasWinWithAllIn) return 1 + GetMinimunBet(totalEarned / 2, maximunAllIn - 1);
+            return 1 + GetMinimunBet(totalEarned - 1, maximunAllIn);
+        }
     }
+
 }
