efc8d6956d9d1349dcdd81a7ee7b6f70654fb52a
Daniel Silva <djimenez@qubeyond.com>
2020-11-18 03:07:59 -0300

    exercise 3

---
+++ TenerifeApp.Test/Exercise3_Test.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TenerifeApp.Test
+{
+    public class Exercise3_Test
+    {
+        [Test]
+        public void Test_AmountConsecutivesMultiplied_1()
+        {
+            var numTest = new Exercise3();
+            var result = numTest.AmountConsecutivesMultiplied(6, 20);
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Test_AmountConsecutivesMultiplied_2()
+        {
+            var numTest = new Exercise3();
+            var result = numTest.AmountConsecutivesMultiplied(21, 29);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Test_AmountConsecutivesMultiplied_3()
+        {
+            var numTest = new Exercise3();
+            var result = numTest.AmountConsecutivesMultiplied(6, 29);
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Test_AmountConsecutivesMultiplied_4()
+        {
+            var numTest = new Exercise3();
+            var result = numTest.AmountConsecutivesMultiplied(6, 30);
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Test_AmountConsecutivesMultiplied_5()
+        {
+            var numTest = new Exercise3();
+            var result = numTest.AmountConsecutivesMultiplied(6, 32);
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Test_AmountConsecutivesMultiplied_6()
+        {
+            var numTest = new Exercise3();
+            var result = numTest.AmountConsecutivesMultiplied(11130, 11775);
+            Assert.AreEqual(4, result);
+        }
+    }
+}
---
+++ TenerifeApp/Exercise3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TenerifeApp
+{
+    public class Exercise3
+    {
+        public int AmountConsecutivesMultiplied(int Min, int Max) 
+        {
+            var ceilingUpper = (int)Math.Ceiling(Math.Sqrt(Max));
+            var upperValue = (ceilingUpper * (ceilingUpper - 1)) > Max ? ceilingUpper - 1 : ceilingUpper;
+
+            var floorDown = (int)Math.Floor(Math.Sqrt(Min));
+            var downValue = (floorDown * (floorDown + 1)) >= Min ? floorDown : floorDown + 1;
+
+            return upperValue - downValue;
+        }
+    }
+}
